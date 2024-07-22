c781cd1924f0233a8dff0490f31af0c8eeb89a71
Tom Pacsó <77162627+PacsoT@users.noreply.github.com>
2024-07-22 14:37:29 +0200

    Update check_and_mail.ps1
    Code tidy up.

--- check_and_mail.ps1
+++ check_and_mail.ps1
@@ -3,14 +3,47 @@
    [switch]$Verbose = $false,
    [switch]$DumpOccurances = $false
 )
+#
+## All your variables are belong to us!
+#
 
+$health_checker_file_location = ".\ExchangeAllServersReport.html"   # This is the location where we dump the HTML report from the Health Checker.
+$runlog_file_location= ".\check_and_mail.log"                       # This is where we log the check_and_mail.ps1 script's activity
+$exceptions_file_location = ".\exceptions.csv"                      # This is where we store our exceptions, so we can ignore some errors we don't want to fix.
+
+$start_key="<td class=""Red"">"                                     # We will comb trough the HTML file searching for this string... 
+$end_key="</td>"                                                    # This marks the end of the stuff we're looking for. We dump everything that is INBETWEEN the start key, and the end key.
+
+
+$custom_certificate_warning_treshold = 10;                          # The health checker usually cries about expiring certificates around 30 days. With this we can set a custom treshold. (Comes handy, when you have Let's Encrypt certs...)
+
+ if($Testing)
+  {
+   $to = "user@testingthecode.hu"                                   # If you are fiddling with the script, or you want to send a copy to your personal address, this is the line to change
+  }
+ else
+  {
+   $to = "team_getting_the_results@exchangeadmins.hu"               # We send the results usually to this address...
+  }
+
+                                                                    # Kind of self explenatory... Mail server, Subject, ports, that kind of stuff...
+
+$From = "adminreport@exchangeadmins.hu"
+$Subject = "Cause for concern was found on the "+$env:COMPUTERNAME+" Exchange server"
+
+$SMTPServer = "localhost"
+$SMTPPort = "25"
+
+
+
+############################################################
+#             Here be dragons                              #
+############################################################
 
 cls
 
-$health_checker_file_location = ".\ExchangeAllServersReport.html"
-$runlog_file_location= ".\check_and_mail.log"
 
-$exceptions_file_location = ".\exceptions.csv"
+
 if (-not(Test-Path $exceptions_file_location -PathType Leaf)) 
  {
   $DumpOccurances=$true
@@ -48,16 +81,12 @@ $total_number_of_occurances = (Select-String -Path $health_checker_file_location
 
 
 
-$start_key="<td class=""Red"">"
-$end_key="</td>"
-$next_start_key_index = 0;
-$next_end_key_index = 1;
-
-$custom_certificate_warning_treshold = 10;     # A health checker 30 nap után nyávog a tanusítvány lejárat előtt, de ezt mi itt felülbírálhatjuk...
 
 $occurances = @();
 $exceptions = @();
 $found_errors = @();
+$next_start_key_index = 0;
+$next_end_key_index = 1;
 
 
 For ($i=0; $i -lt $total_number_of_occurances; $i++)
@@ -102,8 +131,8 @@ if($DumpOccurances)
  }
 
 
-$exceptions_obj= Import-Csv -Path $exceptions_file_location
-foreach ($exc in $exceptions_obj) {$exceptions+=$exc.Name}
+$exceptions_obj= Import-Csv -Path $exceptions_file_location -Header "Exception"
+foreach ($exc in $exceptions_obj) {$exceptions+=$exc.Exception}
 $we_found_new_errors = $false;
 
 For ($i=0; $i -lt $total_number_of_occurances; $i++)
@@ -152,30 +181,17 @@ if($we_found_new_errors)
    {
     Write-host "We're gon' send an e-mail, because we found some new errors!"
    }
-
-  $From = "adminreport@smp.hu"
-  $Subject = "Cause for concern was found on the "+$env:COMPUTERNAME+" Exchange server"
-
-  $SMTPServer = "localhost"
-  $SMTPPort = "25"
-
   
 
   
-  if($Testing)
-   {
-    $to = "tamas.pacso@xxxxxxxxxxxxx.hu"
-   }
-  else
-   {
-    $to = "support@xxxxxxxxxxxxxxxx.net"
-   }
+
 
 # From here we start assembling the e-mails HTML body.
 
    $body = "<h2 style=""font-family:Corbel""> <b>Hi!</b> Unfortunatelly I found some errors on the "+$env:COMPUTERNAME+" Exchange server! </h2>"
+   $body+= "<p style=""font-family:Corbel"">We have choosen to igner certificate warnings until they are " + $custom_certificate_warning_treshold + " days from expiration.</br></p>"
    $body+= "<p style=""font-family:Corbel"">Here is a list of the concerning messages I extracted from the Health Checker's HTML report...</p>"
-   $body+= "  <ul>"
+   $body+= "  <ul>" 
              
 
 
@@ -183,11 +199,13 @@ if($we_found_new_errors)
    {
     $body+= "<li style=""font-family:'Century Gothic';color:#993333"">"+ $err +"</li>"
    }
+  $body+="</ul>"
 
-  $body+="</ul>
-  <p style=""font-family:Corbel; padding-top:30px; align:center"">And here is the complete list of texts we have choosen to ignore: </p>
-  <ul>
-  "
+  $body+= "<p style=""font-family:Corbel""If you see numbers as ""Concerning messages"" that is most likely reffer to certificates closing on to their expiration date. </p>"
+
+  $body+= "<p style=""font-family:Corbel; padding-top:30px; align:center"">And here is the complete list of texts we have choosen to ignore: </p>"
+  $body+= "<ul>"
+
 
 
   foreach($exc in $exceptions)
@@ -195,19 +213,32 @@ if($we_found_new_errors)
     $body+= "<li style=""font-family:'Century Gothic';color:#444444"">"+ $exc +"</li>"
    }
   $body+="</ul>
-  <p style=""font-family:Corbel; text-align: center;"">Attached you can find the complete report, please do have a look!</p>
+  <p style=""font-family:Corbel; paddig-left:30px;"">Attached you can find the complete report, please do have a look!</p>
   "
+
   $log_entry  =  Get-Date -Format "yyyy.mm.dd HH:mm:ss"
-  $log_entry  += 'Sending e-mail, then exiting.';
+  $log_entry  += ' Trying to send an e-mail to: '+$to;
   Add-Content -Path $runlog_file_location -Value $log_entry
+
+  try
+   {
+    Send-MailMessage -From $From -to $To -Subject $Subject -Body $body -BodyAsHtml -SmtpServer $SMTPServer -Port $SMTPPort  -Encoding UTF8 -Attachments $health_checker_file_location
+    $log_entry  =  Get-Date -Format "yyyy.mm.dd HH:mm:ss"
+    $log_entry  += ' Sent an e-mail to '+$to+'. Exiting.';
+    Add-Content -Path $runlog_file_location -Value $log_entry
+   }
+  catch
+   {
+    $log_entry  =  Get-Date -Format "yyyy.mm.dd HH:mm:ss"
+    $log_entry  += " Couldn't send the e-mail! ";
+    Add-Content -Path $runlog_file_location -Value $log_entry
+   }
   
-  Send-MailMessage -From $From -to $To -Subject $Subject -Body $body -BodyAsHtml -SmtpServer $SMTPServer -Port $SMTPPort  -Encoding UTF8 -Attachments $health_checker_file_location
- 
  
  } 
 else
  {
-  Write-Host "We did not find any errors on the exchange system, or all errors have been excused in the exceptions file. Hurray!"
+  Write-Host "We did not find any errors on the Exchange system, or all errors have been excused in the exceptions file. Hurray!"
 
   $log_entry   = Get-Date -Format "yyyy.mm.dd HH:mm:ss"
   $log_entry  += ' No errors, or all have been exused... exiting.';
